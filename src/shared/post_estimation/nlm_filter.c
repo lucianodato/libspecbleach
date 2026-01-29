c75b0dca296739d90895169b952a43a5f96b8d78
Luciano Dato <lucianodato@gmail.com>
2026-01-29 19:02:37 +0100

    Misc

--- src/shared/post_estimation/nlm_filter.c
+++ src/shared/post_estimation/nlm_filter.c
@@ -24,8 +24,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #include <stdlib.h>
 #include <string.h>
 
-// NOTE: Now using global configuration from configurations.h
-
 struct NlmFilter {
   NlmFilterConfig config;
 
