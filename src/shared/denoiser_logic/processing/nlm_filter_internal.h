e7186d8274ab74f11b08f56901d90025f6717eb2
Luciano Dato <lucianodato@gmail.com>
2026-07-31 21:35:44 +0200

    Fix linting again

--- src/shared/denoiser_logic/processing/nlm_filter_internal.h
+++ src/shared/denoiser_logic/processing/nlm_filter_internal.h
@@ -263,7 +263,7 @@ static inline __attribute__((unused)) bool nlm_filter_process_core(
     }
 
     for (int32_t dt = -search_time_past; dt <= search_time_future; dt++) {
-      float* cand_rows[8];
+      float* cand_rows[8] = {NULL};
       if (filter->config.patch_size == 8) {
         for (int r = 0; r < 8; r++) {
           cand_rows[r] = cached_get_frame(filter, dt + r - 4);
